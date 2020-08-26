2161e001e03218a6c8c07ba561a61e8391289d4b
zaxbpc0 <touchme@wo.cn>
2020-08-26 23:18:54 +0800

    update via Git

--- blog/weather/he-simple-com-pc.js
+++ blog/weather/he-simple-com-pc.js
@@ -3,7 +3,7 @@
   c.rel = 'stylesheet'
   c.href = 'https://cdn.jsdelivr.net/gh/zaxbpc0/img/blog/weather/he-simple.css'
   var s = d.createElement('script')
-  s.src = 'https://cdn.jsdelivr.net/gh/zaxbpc0/img/blog/weather/he-simple.js'
+  s.src = 'https://cdn.jsdelivr.net/gh/zaxbpc0/img/blog/weather/he-simple-pc.js'
   var sn = d.getElementsByTagName('script')[0]
   sn.parentNode.insertBefore(c, sn)
   sn.parentNode.insertBefore(s, sn)
