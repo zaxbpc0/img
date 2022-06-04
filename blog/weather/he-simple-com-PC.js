84093b03732abc3db6fcf652f05442e262788f07
zaxbpc0 <63100307+zaxbpc0@users.noreply.github.com>
2022-06-04 21:33:27 +0800

    Update and rename he-simple-com-pc.js to he-simple-com-PC.js

--- blog/weather/he-simple-com-PC.js
+++ blog/weather/he-simple-com-PC.js
@@ -1,9 +1,9 @@
 (function (d) {
   var c = d.createElement('link')
   c.rel = 'stylesheet'
-  c.href = 'https://cdn.jsdelivr.net/gh/zaxbpc0/img/blog/weather/he-simple.css'
+  c.href = 'https://fastly.jsdelivr.net/gh/zaxbpc0/img/blog/weather/he-simple.css'
   var s = d.createElement('script')
-  s.src = 'https://cdn.jsdelivr.net/gh/zaxbpc0/img/blog/weather/he-simple-pc.js'
+  s.src = 'https://fastly.jsdelivr.net/gh/zaxbpc0/img/blog/weather/he-simple-pc.js'
   var sn = d.getElementsByTagName('script')[0]
   sn.parentNode.insertBefore(c, sn)
   sn.parentNode.insertBefore(s, sn)
